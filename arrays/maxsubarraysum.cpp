3de1e1b3ef109155871ef952c8b649121f24effb
SravanKumarBattu <sravankumar_b19@vnrvjiet.in>
2021-12-18 20:35:58 +0530

    day 01

--- arrays/maxsubarraysum.cpp
+++ arrays/maxsubarraysum.cpp
@@ -6,7 +6,7 @@ int maxsubsum(int arr[],int n);
 
 int main()
 {
-    int arr[]={10,20,30,40,50};
+    int arr[]={10,-20,30,-40,50};
     //(10,10)(10,20),(10,30),....(20,20),(20,30),...(30,30),...(50,50)
     int n=sizeof(arr)/sizeof(int);
 
@@ -20,15 +20,17 @@ int maxsubsum(int arr[],int n)
     int sum=0,max=0;
     for(int i=0;i<n;i++)
     {
-        for(int j=i+1;j<n;j++)
+        for(int j=i;j<n;j++)
         //including the number itself(10,10),(20,20),... replace j=i+1 with j=i 
         {
+            for(int k=i;k<j;k++)
+            {
             sum=arr[i]+arr[j];
             if(sum>max)
             {
                 max=sum;
             }
-           
+            }
         }
     }
     return max;
